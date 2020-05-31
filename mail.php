9c6b8646e6d8f53c45085600eba2fa02fa294dea
Arthur Carl Forest-Jones <carlfj@live.co.uk>
2020-05-31 19:14:51 +0100

    Update mail.php to include thankyou.html page

--- mail.php
+++ mail.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/main.css">
+</head>
+</html>
 <?php
 $name = $_POST['name'];
 $email = $_POST['email'];
@@ -7,5 +15,13 @@ $recipient = "admin@heydaysfurniture.co.uk";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='home.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+echo "<section class='landing_page'>
+<section class='thank_container'>
+    <div class='thank_title'>Thank you for your message!</div>
+    <div class='thank_content'>I will get back to you as soon as possible.</div><br>
+    <a href='home.html' class='thank_home'>Back to site</a>
+</section>
+</section>
+";
 ?>
+
